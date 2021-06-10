0a926fef7e96fe00f0ca7ea17175599fb567ae43
nicos <nico.sesma@gmail.com>
2021-06-10 23:08:44 +0200

    bo menus

--- src/api/routes/boulle-bo-apis.ts
+++ src/api/routes/boulle-bo-apis.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response, Router } from 'express';
 
+import Logger from '../../loaders/logger';
+
 const rimraf = require("rimraf");
 const fsPromises = require('fs').promises;
 const path = require('path');
@@ -23,12 +25,45 @@ export default (app: Router) => {
   );
 
   route.get(
-    '/header-menu',
+    '/menu',
     async (req: Request, res: Response, next: NextFunction) => {
       try {
-        let pathFileDraws = __dirname + '/../../public/ressources/boulle-bo/header-menu.json'
+        let pathFileDraws = __dirname + '/../../public/ressources/boulle-bo/menu.json'
 
         let rawdata = fs.readFileSync(pathFileDraws);
+
+        return res.status(201).json(JSON.parse(rawdata));
+
+      } catch (e) {
+        return next(e);
+      }
+    },
+  );
+
+  route.post(
+    '/menu',
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const {nom, url} = req.body
+        let menuEntries: any[] = []
+
+        Logger.debug(`ajout d'une nouvelle entrée de menu...`);
+
+        let pathFile = __dirname + '/../../public/ressources/boulle-bo/menu.json'
+
+        if (!fs.existsSync(pathFile)) {
+          fs.writeFileSync(pathFile, '[]');
+        } else {
+          let datas = fs.readFileSync(pathFile);
+
+          menuEntries = JSON.parse(datas);
+        }
+
+        menuEntries.push({label: nom, linkTo: url})
+
+        fs.writeFileSync(pathFile,  JSON.stringify(menuEntries));
+
+        let rawdata = fs.readFileSync(pathFile);
 
         return res.status(201).json(JSON.parse(rawdata));
 
